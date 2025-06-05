0451bee4455c3c9b02d2acee0b4db21fa257341b
Trupti Bhamare <trupti.bhamare015@gmail.com>
2025-06-05 16:05:56 +0200

    Initialising project

---
+++ src/main/java/com/_global/webservice/devicehub/DevicehubApplication.java
@@ -0,0 +1,13 @@
+package com._global.webservice.devicehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_global/webservice/devicehub/DevicehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com._global.webservice.devicehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicehubApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
